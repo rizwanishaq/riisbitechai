07a6309f969e8689860a6958eadfd82904d71470
rizwanishaq <rizwanishaq@gmail.com>
2023-05-22 16:11:32 +0200

    modified

--- client/src/pages/MachineLearning/Speech2Text/Speech2Text.jsx
+++ client/src/pages/MachineLearning/Speech2Text/Speech2Text.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, ListGroup, ListGroupItem } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Nav from "react-bootstrap/Nav";
 import { toast } from "react-toastify";
 import uuid from "react-uuid";
+// import Select from "react-select";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
@@ -21,6 +22,14 @@ const Speech2Text = () => {
   } = useSpeechRecognition();
   const { speak } = useSpeechSynthesis();
   const [notes, setNotes] = useState([]);
+
+  // const [language, setLanguage] = useState("es-ES");
+
+  // const options = [
+  //   { value: "ar-EG", label: "Chocolate" },
+  //   { value: "en-UK", label: "Strawberry" },
+  //   { value: "en-US", label: "Vanilla" },
+  // ];
 
   useEffect(() => {
     if (!listening && transcript !== "") {
@@ -44,7 +53,7 @@ const Speech2Text = () => {
   return (
     <main id="main" className="main">
       <div className="pagetitle">
-        <h1>Voice Note</h1>
+        <h1>Voice Note </h1>
         <Nav>
           <li className="breadcrumb-item">
             The speech-to-text feature in the Chrome browser is a powerful tool
@@ -86,7 +95,7 @@ const Speech2Text = () => {
                           onClick={() => {
                             SpeechRecognition.startListening({
                               language: "en-US",
-                              //   continuous: true,
+                              // continuous: true,
                             });
                             toast.success("Started listening");
                           }}
@@ -94,16 +103,26 @@ const Speech2Text = () => {
                       </>
                     )}
                   </p>
+                  {/* <Select
+                    options={options}
+                    onChange={(e) => setLanguage(e.value)}
+                  /> */}
                 </Card.Title>
               </Card.Body>
             </Card>
           </Col>
-          <Col>
-            {/* <ListGroup>
-            {notes.map((note) => (
-              <VoiceNote key={note._id} note={note} />
-            ))}
-          </ListGroup> */}
+          <Col lg={6}>
+            <Card>
+              <Card.Body>
+                <ListGroup>
+                  {notes.map((note) => (
+                    <ListGroupItem className="text-center" key={note._id}>
+                      {note.text}
+                    </ListGroupItem>
+                  ))}
+                </ListGroup>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </section>
